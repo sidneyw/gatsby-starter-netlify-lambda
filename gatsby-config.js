13fcc76683c41aa076fb2efd292ae4bcef2fbf08
Sidneyw <sidney.wijngaarde1@ibm.com>
2018-04-09 20:35:24 -0400

    Proxy Path Explanation

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Netlify Lambda',
   },
+  // The gatsby proxy will proxy requests starting with `prefix` to
+  // the path `${url}${prefix}`. Therefore any request that goes to
+  // /localhost:9000 in development goes to http://localhost:9000 which is where
+  // the development lambda serve is located by default.
+
+  // When hosting lambda functions in production on netlify the functions will
+  // be available under the same domain at `${your-domain.tld}/.netlify/functions`
+  // and so CORS in no longer an issue
   proxy: {
     prefix: '/localhost:9000',
     url: 'http:/',
